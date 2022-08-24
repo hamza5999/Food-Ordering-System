88fa9bac4072ec6422889aacc6d0b113893e577b
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-24 18:54:30 +0500

    Add ServiceCharge model

---
+++ db/migrate/20220824133723_create_service_charges.rb
@@ -0,0 +1,9 @@
+class CreateServiceCharges < ActiveRecord::Migration[6.1]
+  def change
+    create_table :service_charges do |t|
+      t.integer :cuttoff
+
+      t.timestamps
+    end
+  end
+end
