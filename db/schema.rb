dcebc58030aee9dafd5971cc36525e7056e7af19
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-25 16:48:40 +0500

    Add MenuTiming model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_110739) do
+ActiveRecord::Schema.define(version: 2022_08_25_114441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2022_08_25_110739) do
 
   create_table "item_groups", force: :cascade do |t|
     t.string "group_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "menu_timings", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
