fe4aded5a0fc621bc025f5a5b5778ff22f0b84ff
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-25 17:43:32 +0500

    Add VoucherTimeline model

---
+++ app/models/voucher_timeline.rb
@@ -0,0 +1,2 @@
+class VoucherTimeline < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_123543) do
+ActiveRecord::Schema.define(version: 2022_08_25_124150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -143,6 +143,13 @@ ActiveRecord::Schema.define(version: 2022_08_25_123543) do
 
   create_table "service_charges", force: :cascade do |t|
     t.integer "cuttoff"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "voucher_timelines", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
