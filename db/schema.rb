afa94af2ed2c712c4b7cdc7a8cdf936e8222916d
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-25 15:22:37 +0500

    Add FoodItem model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_101307) do
+ActiveRecord::Schema.define(version: 2022_08_25_102022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2022_08_25_101307) do
 
   create_table "admin_charges", force: :cascade do |t|
     t.decimal "admin_amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "food_items", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
