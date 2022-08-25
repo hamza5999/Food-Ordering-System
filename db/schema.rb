af7c0142a9e94889aaa2c49f8618e12f1431c853
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-25 15:58:13 +0500

    Add Rating model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_104933) do
+ActiveRecord::Schema.define(version: 2022_08_25_105611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,13 @@ ActiveRecord::Schema.define(version: 2022_08_25_104933) do
   create_table "payments", force: :cascade do |t|
     t.decimal "total_amount"
     t.datetime "payment_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "ratings", force: :cascade do |t|
+    t.string "description"
+    t.integer "value"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
