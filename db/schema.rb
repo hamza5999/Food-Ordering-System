db27a925dd01ca29d1d7c977cbf3be369791c428
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-25 16:55:43 +0500

    Add Deal model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_114441) do
+ActiveRecord::Schema.define(version: 2022_08_25_115240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2022_08_25_114441) do
 
   create_table "admin_charges", force: :cascade do |t|
     t.decimal "admin_amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "deals", force: :cascade do |t|
+    t.string "deal_name"
+    t.decimal "deal_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
