3630e6723195149fec01b5ae011e39bc967d6264
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-25 13:03:20 +0500

    Add OrderItem model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_075658) do
+ActiveRecord::Schema.define(version: 2022_08_25_080209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2022_08_25_075658) do
 
   create_table "admin_charges", force: :cascade do |t|
     t.decimal "admin_amount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "order_items", force: :cascade do |t|
+    t.integer "quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
