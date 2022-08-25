fd0bdcf3f097d94876f6951cd9038293d6e1a156
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-25 12:22:13 +0500

    Add Order model

---
+++ db/migrate/20220825071952_create_orders.rb
@@ -0,0 +1,12 @@
+class CreateOrders < ActiveRecord::Migration[6.1]
+  def change
+    create_table :orders do |t|
+      t.datetime :order_time
+      t.string :delivery_address
+      t.string :order_status
+      t.decimal :total_price
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_062453) do
+ActiveRecord::Schema.define(version: 2022_08_25_071952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "addresses", force: :cascade do |t|
     t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "order_time"
+    t.string "delivery_address"
+    t.string "order_status"
+    t.decimal "total_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
