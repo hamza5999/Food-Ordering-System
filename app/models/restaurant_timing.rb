df059cdb7f10eb7c77b3121bbf9864080cbf1560
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-25 14:56:30 +0500

    Add RestaurantTiming model

---
+++ app/models/restaurant_timing.rb
@@ -0,0 +1,2 @@
+class RestaurantTiming < ApplicationRecord
+end
---
+++ db/migrate/20220825095441_create_restaurant_timings.rb
@@ -0,0 +1,10 @@
+class CreateRestaurantTimings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :restaurant_timings do |t|
+      t.timestamp :start_time
+      t.timestamp :end_time
+
+      t.timestamps
+    end
+  end
+end
