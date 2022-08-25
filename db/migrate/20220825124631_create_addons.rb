85ec02cf1f8c3962a66fcc40184965153bdf8ca4
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-25 17:48:24 +0500

    Add Addon model

---
+++ db/migrate/20220825124631_create_addons.rb
@@ -0,0 +1,10 @@
+class CreateAddons < ActiveRecord::Migration[6.1]
+  def change
+    create_table :addons do |t|
+      t.string :name
+      t.integer :quantity
+
+      t.timestamps
+    end
+  end
+end
