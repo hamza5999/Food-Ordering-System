b1d9b48f2b9e6705d85921253861a37eb6999726
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-26 15:27:19 +0500

    Add restaurant ref to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_25_124631) do
+ActiveRecord::Schema.define(version: 2022_08_26_102044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,6 +110,8 @@ ActiveRecord::Schema.define(version: 2022_08_25_124631) do
     t.decimal "total_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "restaurant_id", null: false
+    t.index ["restaurant_id"], name: "index_orders_on_restaurant_id"
   end
 
   create_table "payments", force: :cascade do |t|
@@ -169,4 +171,5 @@ ActiveRecord::Schema.define(version: 2022_08_25_124631) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "orders", "restaurants"
 end
