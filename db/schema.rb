3226f8330492ce5752a4cffa47249534a31775a7
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-29 12:38:20 +0500

    Add customer ref to addresses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_29_065229) do
+ActiveRecord::Schema.define(version: 2022_08_29_073501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2022_08_29_065229) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "customer_id", null: false
+    t.index ["customer_id"], name: "index_addresses_on_customer_id"
   end
 
   create_table "admin_charges", force: :cascade do |t|
@@ -211,6 +213,7 @@ ActiveRecord::Schema.define(version: 2022_08_29_065229) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "addresses", "customers"
   add_foreign_key "orders", "customers"
   add_foreign_key "orders", "restaurants"
   add_foreign_key "orders", "vouchers"
