26d568f39ce5baafd9737208fe903e00dd75a26d
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-29 15:33:02 +0500

    Add self join ref to employees

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,5 +1,9 @@
 class Employee < ApplicationRecord
   has_many :orders
+  has_many :subordinates, class_name: "Employee",
+                          foreign_key: "manager_id"
+
+  belongs_to :manager, class_name: "Employee", optional: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
