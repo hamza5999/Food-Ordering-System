9dfe53f44fc37ae033d33edcf726615aea1f7a3e
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-30 13:42:46 +0500

    Remove status from discount

---
+++ db/migrate/20220830082253_remove_status_from_discounts.rb
@@ -0,0 +1,5 @@
+class RemoveStatusFromDiscounts < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :discounts, :status, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_30_081553) do
+ActiveRecord::Schema.define(version: 2022_08_30_082253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,7 +81,6 @@ ActiveRecord::Schema.define(version: 2022_08_30_081553) do
 
   create_table "discounts", force: :cascade do |t|
     t.string "name"
-    t.string "status"
     t.decimal "value"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
