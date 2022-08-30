f705f47aed89d2119a5eca2a74c61a6019785778
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-30 10:26:30 +0500

    Add order ref to restaurantdues

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,5 @@ class Order < ApplicationRecord
   belongs_to :customer
   belongs_to :employee
   has_one :admin_charge
+  has_one :restaurant_due
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_29_132226) do
+ActiveRecord::Schema.define(version: 2022_08_30_051530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -184,6 +184,8 @@ ActiveRecord::Schema.define(version: 2022_08_29_132226) do
     t.decimal "restaurant_dues"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "order_id", null: false
+    t.index ["order_id"], name: "index_restaurant_dues_on_order_id"
   end
 
   create_table "restaurant_timings", force: :cascade do |t|
@@ -232,4 +234,5 @@ ActiveRecord::Schema.define(version: 2022_08_29_132226) do
   add_foreign_key "orders", "restaurants"
   add_foreign_key "orders", "vouchers"
   add_foreign_key "ratings", "customers"
+  add_foreign_key "restaurant_dues", "orders"
 end
