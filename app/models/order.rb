3370849c594b6f680a5d8325ebfbbdd4e6addd60
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-30 18:08:46 +0500

    Add associations between Order and OrderItem model

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,11 @@ class Order < ApplicationRecord
   belongs_to :voucher
   belongs_to :customer
   belongs_to :employee
+
   has_one :admin_charge
   has_one :restaurant_due
   has_one :payment
   has_one :rating
+
+  has_many :order_items
 end
