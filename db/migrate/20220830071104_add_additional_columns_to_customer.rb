5c27ac94adb95b0c5f534f2227e6fffa13857a83
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-30 12:21:50 +0500

    Add customer remaining columns

---
+++ db/migrate/20220830071104_add_additional_columns_to_customer.rb
@@ -0,0 +1,7 @@
+class AddAdditionalColumnsToCustomer < ActiveRecord::Migration[6.1]
+  def change
+    add_column :customers, :first_name, :string
+    add_column :customers, :last_name, :string
+    add_column :customers, :phone, :string
+  end
+end
