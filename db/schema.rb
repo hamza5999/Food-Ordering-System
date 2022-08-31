3260fed04fdde80914c9b3e242312cd1cf2bd853
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-31 11:15:52 +0500

    Add restaurant ref to restaurant_timings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_30_122645) do
+ActiveRecord::Schema.define(version: 2022_08_31_055847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -203,6 +203,8 @@ ActiveRecord::Schema.define(version: 2022_08_30_122645) do
     t.datetime "end_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "restaurant_id", null: false
+    t.index ["restaurant_id"], name: "index_restaurant_timings_on_restaurant_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -248,4 +250,5 @@ ActiveRecord::Schema.define(version: 2022_08_30_122645) do
   add_foreign_key "ratings", "customers"
   add_foreign_key "ratings", "orders"
   add_foreign_key "restaurant_dues", "orders"
+  add_foreign_key "restaurant_timings", "restaurants"
 end
