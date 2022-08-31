bd7edd93f097c38ad120f216e3b278dcc1005498
hamza5999 <mhamzamahmood5@gmail.com>
2022-08-31 19:18:08 +0500

    Add addon and menu_item mid table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_31_140112) do
+ActiveRecord::Schema.define(version: 2022_08_31_141339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 2022_08_31_140112) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "restaurant_id", null: false
     t.index ["restaurant_id"], name: "index_addons_on_restaurant_id"
+  end
+
+  create_table "addons_menu_items", id: false, force: :cascade do |t|
+    t.bigint "menu_item_id", null: false
+    t.bigint "addon_id", null: false
   end
 
   create_table "addresses", force: :cascade do |t|
