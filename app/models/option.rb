9dea93148d4dab766114547d30128157115fb10f
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-01 15:51:20 +0500

    Add option, order_item, option_order_item association

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,6 +1,9 @@
 class Option < ApplicationRecord
   belongs_to :restaurant
   belongs_to :item_group
+
   has_one :deal_item
-  has_and_belongs_to_many :order_items
+
+  has_many :option_order_items
+  has_many :order_items, through: :option_order_items
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,6 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
-  has_and_belongs_to_many :options
+
+  has_many :option_order_items
+  has_many :options, through: :option_order_items
 end
