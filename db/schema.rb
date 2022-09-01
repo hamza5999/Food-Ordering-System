8d87140d31e2594f9c7f6e7fbe8239029715d708
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-01 13:19:44 +0500

    Rename menu_items_menus table to menu_item_menus

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_01_071854) do
+ActiveRecord::Schema.define(version: 2022_09_01_081320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -144,17 +144,17 @@ ActiveRecord::Schema.define(version: 2022_09_01_071854) do
     t.string "name"
   end
 
+  create_table "menu_item_menus", id: false, force: :cascade do |t|
+    t.bigint "menu_id", null: false
+    t.bigint "menu_item_id", null: false
+  end
+
   create_table "menu_items", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "restaurant_id", null: false
     t.string "name"
     t.index ["restaurant_id"], name: "index_menu_items_on_restaurant_id"
-  end
-
-  create_table "menu_items_menus", id: false, force: :cascade do |t|
-    t.bigint "menu_id", null: false
-    t.bigint "menu_item_id", null: false
   end
 
   create_table "menu_timings", force: :cascade do |t|
