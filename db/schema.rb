4f9a92770c0dce4711958c34f56b37fd5b5db1ce
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-06 10:26:33 +0500

    Drop admin table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_02_132945) do
+ActiveRecord::Schema.define(version: 2022_09_06_052402) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,18 +48,6 @@ ActiveRecord::Schema.define(version: 2022_09_02_132945) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "order_id", null: false
     t.index ["order_id"], name: "index_admin_charges_on_order_id"
-  end
-
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "customer_vouchers", id: false, force: :cascade do |t|
