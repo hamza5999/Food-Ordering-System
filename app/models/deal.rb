d5b8330c15da590f42c9d248c108817fe10c92b5
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-06 17:41:15 +0500

    Add menuable concern in models

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,8 +1,7 @@
 class Deal < ApplicationRecord
   include OrderableConcern
+  include MenuableConcern
 
   has_many :deal_item_deals
   has_many :deal_items, through: :deal_item_deals
-
-  has_many :menu_items, as: :menuable
 end
--- app/models/item_group.rb
+++ app/models/item_group.rb
@@ -1,6 +1,7 @@
 class ItemGroup < ApplicationRecord
+  include MenuableConcern
+
   has_many :food_items
   has_many :options
-  has_many :menu_items, as: :menuable
   has_many :deal_items, as: :dealable
 end
