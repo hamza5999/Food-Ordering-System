938b59487907da1515c9cfc7a786374efea94e40
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-07 17:29:31 +0500

    Add restaurant resource in ActiveAdmin dashboard

---
+++ app/admin/restaurants.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Restaurant do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :location, :timings, :availability
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :location, :timings, :availability]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
