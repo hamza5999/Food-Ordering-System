612e244d4acb8f1cfbd05e93883fb8173ca31748
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-07 18:43:37 +0500

    Customize index of customer resource in admin panel

--- app/admin/customers.rb
+++ app/admin/customers.rb
@@ -1,18 +1,10 @@
 ActiveAdmin.register Customer do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :first_name, :last_name, :phone
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :first_name, :last_name, :phone]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    id_column
+    column :first_name
+    column :last_name
+    column :phone
+    column :email
+    actions
+  end
 end
