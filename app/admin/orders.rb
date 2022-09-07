ec739ec3d7a2f269e235f1824812ab89d6907901
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-07 19:30:12 +0500

    Customize index of order resource in admin panel

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,18 +1,12 @@
 ActiveAdmin.register Order do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :order_time, :delivery_address, :total_price, :restaurant_id, :voucher_id, :customer_id, :employee_id, :status
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:order_time, :delivery_address, :total_price, :restaurant_id, :voucher_id, :customer_id, :employee_id, :status]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    id_column
+    column :customer
+    column :order_time
+    column :delivery_address
+    column :restaurant
+    column :total_price
+    column :status
+    actions
+  end
 end
