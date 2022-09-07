aa186fccfcb06beefc8d6c8f98d28546bd0f4dc7
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-07 19:39:31 +0500

    Customize index of restaurant resource in admin panel

--- app/admin/restaurants.rb
+++ app/admin/restaurants.rb
@@ -1,18 +1,10 @@
 ActiveAdmin.register Restaurant do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :location, :timings, :availability
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :location, :timings, :availability]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    id_column
+    column :name
+    column :timings
+    column :location
+    column :availability
+    actions
+  end
 end
