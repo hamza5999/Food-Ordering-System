63211ac04700f2684d56418bb6b4acf6d6c65b36
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-07 19:45:47 +0500

    Customize filter of restaurant resource in admin panel

--- app/admin/restaurants.rb
+++ app/admin/restaurants.rb
@@ -1,4 +1,10 @@
 ActiveAdmin.register Restaurant do
+  filter :name
+  filter :location
+  filter :menus
+  filter :discounts
+  filter :restaurant_timings
+
   index do
     id_column
     column :name
