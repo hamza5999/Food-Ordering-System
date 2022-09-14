ecabc1a5d9ed2d357d5a50a6faab21b9e6b1ae59
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-14 14:00:58 +0500

    Customize index in AdminCharges resource of ActiveAdmin

--- app/admin/admin_charges.rb
+++ app/admin/admin_charges.rb
@@ -1,18 +1,8 @@
 ActiveAdmin.register AdminCharge do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :admin_amount, :order_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:admin_amount, :order_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    id_column
+    column :admin_amount
+    column :order_id
+    actions
+  end
 end
