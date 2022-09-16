33259c35b5c550d9ec48ca29d8ca0b42624a0166
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-16 17:35:02 +0500

    Update route for employee invitation controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :customers
-  devise_for :employees
+  devise_for :employees, controllers: { invitations: 'employees/invitations' }
 
   root 'home#index'
   get 'home/index'
