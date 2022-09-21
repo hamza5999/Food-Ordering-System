c3de7842bd316615582a4c1c76c497969b582529
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-21 12:21:51 +0500

    Permit params of ItemGroup and Options in ItemGroup controller

--- app/controllers/item_groups_controller.rb
+++ app/controllers/item_groups_controller.rb
@@ -7,4 +7,10 @@ class ItemGroupsController < ApplicationController
     @item_group = ItemGroup.new
     @options = @item_group.options.build
   end
+
+  private
+
+  def item_group_params
+    params.require(:item_group).permit(:name, options_attributes: [:id, :name, :restaurant_id, :item_group_id, :_destroy])
+  end
 end
