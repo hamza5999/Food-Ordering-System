fae5b520e1f57ed5cd148ac3226d63d2f8fe0d95
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-22 10:11:41 +0500

    Update ItemGroup model to accept nested option values

--- app/models/item_group.rb
+++ app/models/item_group.rb
@@ -3,7 +3,7 @@ class ItemGroup < ApplicationRecord
   include DealableConcern
 
   has_many :food_items
-  has_many :options
+  has_many :options, inverse_of: :item_group
 
-  accepts_nested_attributes_for :options
+  accepts_nested_attributes_for :options, reject_if: :all_blank, allow_destroy: true
 end
