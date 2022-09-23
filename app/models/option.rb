e9c85054200343649eb1adcd7a88b421ae8569c7
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-23 16:00:57 +0500

    Remove DealItem association from Option model

--- app/models/option.rb
+++ app/models/option.rb
@@ -2,8 +2,6 @@ class Option < ApplicationRecord
   belongs_to :restaurant
   belongs_to :item_group
 
-  has_one :deal_item
-
   has_many :option_order_items
   has_many :order_items, through: :option_order_items
 
