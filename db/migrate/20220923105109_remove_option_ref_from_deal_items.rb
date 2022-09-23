eef8ab200ee3b2cc989c1c2f92c13a499a31ac5c
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-23 15:54:23 +0500

    Remove option ref from DealItems table

---
+++ db/migrate/20220923105109_remove_option_ref_from_deal_items.rb
@@ -0,0 +1,5 @@
+class RemoveOptionRefFromDealItems < ActiveRecord::Migration[6.1]
+  def change
+    remove_reference :deal_items, :option, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_15_075558) do
+ActiveRecord::Schema.define(version: 2022_09_23_105109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,10 +105,8 @@ ActiveRecord::Schema.define(version: 2022_09_15_075558) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "option_id", null: false
     t.string "dealable_type"
     t.integer "dealable_id"
-    t.index ["option_id"], name: "index_deal_items_on_option_id"
   end
 
   create_table "deals", force: :cascade do |t|
@@ -351,7 +349,6 @@ ActiveRecord::Schema.define(version: 2022_09_15_075558) do
   add_foreign_key "addons", "restaurants"
   add_foreign_key "addresses", "customers"
   add_foreign_key "admin_charges", "orders"
-  add_foreign_key "deal_items", "options"
   add_foreign_key "discount_timelines", "discounts"
   add_foreign_key "discounts", "restaurants"
   add_foreign_key "employees", "employees", column: "manager_id"
