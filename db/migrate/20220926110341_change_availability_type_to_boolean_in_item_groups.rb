150045de2d93bb1fdc516a2b61b64ffa10b71d98
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-26 16:08:52 +0500

    Change availability type from enum to bool in ItemGroups

---
+++ db/migrate/20220926110341_change_availability_type_to_boolean_in_item_groups.rb
@@ -0,0 +1,6 @@
+class ChangeAvailabilityTypeToBooleanInItemGroups < ActiveRecord::Migration[6.1]
+  def change
+    remove_column :item_groups, :availability, :integer
+    add_column :item_groups, :availability, :boolean, default: 1
+  end
+end
