6a2ecc98866a0bce95830c3aac0a3726587e33fd
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-28 17:54:35 +0500

    Add destroy action in Discounts controller

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -6,4 +6,17 @@ class DiscountsController < ApplicationController
   def show
     @discount = Discount.find(params[:id])
   end
+
+  def destroy
+    @discount = Discount.find(params[:id])
+    if @discount.destroy
+      flash[:notice] = 'Discount was deleted successfully.'
+      redirect_to discounts_path
+    else
+      message = @item_group.errors.full_messages.first
+		  flash[:alert] = "Error: " + message
+      redirect_to discounts_path
+    end
+  end
+
 end
