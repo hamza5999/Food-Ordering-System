1220afee9d2663cdd39fb77debc6c721d22c8d23
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-29 19:56:43 +0500

    Remove all,unavailable categories routes from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,4 @@ Rails.application.routes.draw do
   devise_for :employees, controllers: { invitations: 'employees/invitations' }
 
   root 'dashboard#index'
-  get "all_categories", to: "item_groups#all_categories"
-  get "categories_unavailable", to: "item_groups#categories_unavailable"
 end
