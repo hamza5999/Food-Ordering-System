af51baaa03cfb56694a12d8bb91a48894bb05e20
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-29 20:02:46 +0500

    Add active_discounts scope in Discount model

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,6 +1,8 @@
 class Discount < ApplicationRecord
   enum kind: {fixed: 0, percentage: 1}
 
+  scope :active_discounts, -> { where(:status => 1)}
+  # Ex:- scope :active, -> {where(:active => true)}
   belongs_to :restaurant
   has_many :food_items
   has_many :discount_timelines
