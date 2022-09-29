82833e3baefb6afbea5ad68012df9f41aba4d3c1
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-29 20:04:41 +0500

    Add expired_discounts scope in Discount model

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,7 +2,8 @@ class Discount < ApplicationRecord
   enum kind: {fixed: 0, percentage: 1}
 
   scope :active_discounts, -> { where(:status => 1)}
-  # Ex:- scope :active, -> {where(:active => true)}
+  scope :expired_discounts, -> {where(:status => 0)}
+
   belongs_to :restaurant
   has_many :food_items
   has_many :discount_timelines
