ef9458a6a70d9a76a8875a290cb07f4f15ecf27e
hamza5999 <mhamzamahmood5@gmail.com>
2022-09-30 19:25:21 +0500

    Convert old syntax of scopes to new one in Discount model

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,8 +1,8 @@
 class Discount < ApplicationRecord
   enum kind: {fixed: 0, percentage: 1}
 
-  scope :active_discounts, -> { where(:status => 1)}
-  scope :expired_discounts, -> {where(:status => 0)}
+  scope :active_discounts, -> { where(status: 1) }
+  scope :expired_discounts, -> { where(status: 0) }
 
   belongs_to :restaurant
   has_many :food_items
