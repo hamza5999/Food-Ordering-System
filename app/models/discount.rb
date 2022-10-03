3a9ce3ed71cb94387ae175598bcaaca3cdf43695
hamza5999 <mhamzamahmood5@gmail.com>
2022-10-03 18:16:59 +0500

    Update discount model to accept nested DiscountTimeline attributes

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -8,6 +8,8 @@ class Discount < ApplicationRecord
   has_many :food_items
   has_many :discount_timelines
 
+  accepts_nested_attributes_for :discount_timelines, reject_if: :all_blank, allow_destroy: true
+
   def self.ransackable_scopes(auth_object = nil)
     %i(active_discounts expired_discounts)
   end
