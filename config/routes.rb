c7aad5bf2315ea58e9157ca28195ae99cc163a08
hamza5999 <mhamzamahmood5@gmail.com>
2022-10-05 18:10:32 +0500

    Add route for discarded action of Discounts controller in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :item_groups
-  resources :discounts
+  resources :discounts do
+    get :discarded, on: :collection
+  end
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :customers
