d7e8a4b2418651ac4c41dc49ff4c99113c02291c
hamza5999 <mhamzamahmood5@gmail.com>
2022-10-10 17:55:21 +0500

    Add optional:true in FoodItem model to allow nil in discounts

--- app/models/food_item.rb
+++ app/models/food_item.rb
@@ -3,7 +3,7 @@ class FoodItem < ApplicationRecord
   include DealableConcern
 
   belongs_to :item_group
-  belongs_to :discount
+  belongs_to :discount, optional: true
 
   has_many :food_item_options
   has_many :options, through: :food_item_options
