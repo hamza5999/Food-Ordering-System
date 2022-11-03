ef70729f32aa7a1955fc2d49c8ee2ad09045a9cd
hamza5999 <mhamzamahmood5@gmail.com>
2022-11-03 15:18:19 +0500

    Add show action in profile controller

--- app/controllers/employees/profile_controller.rb
+++ app/controllers/employees/profile_controller.rb
@@ -3,5 +3,7 @@
 # The show action of the ProfileController of the Employees namespace will find an employee by id and
 # assign it to the @employee instance variable.
 class Employees::ProfileController < ApplicationController
-  def show; end
+  def show
+    @employee = current_employee
+  end
 end
