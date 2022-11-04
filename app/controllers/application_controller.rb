3a6d50b561356175651531ced8beae2e09105e4b
hamza5999 <mhamzamahmood5@gmail.com>
2022-11-04 18:48:13 +0500

    Permit params for edit employee view in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@
 # Before any action is performed, authenticate the employee.
 class ApplicationController < ActionController::Base
   before_action :authenticate_employee!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name phone])
+  end
 end
