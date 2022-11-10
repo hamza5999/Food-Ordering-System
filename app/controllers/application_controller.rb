13a84a4c0196caa1a10dd2451de4aa6eccb9f327
hamza5999 <mhamzamahmood5@gmail.com>
2022-11-10 18:06:37 +0500

    Add profile pic in parmitted params of devise in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name phone])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name phone profile_pic])
   end
 end
