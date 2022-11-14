7546e9a6d14d5e9afc6b8aa7c2949807293e019f
hamza5999 <mhamzamahmood5@gmail.com>
2022-11-14 19:36:14 +0500

    Add edit_employee_email method in EmployeeMailer

--- app/mailers/employee_mailer.rb
+++ app/mailers/employee_mailer.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
+# The EmployeeMailer class inherits from the ApplicationMailer class, and has a method called
+# employee_welcome_email that takes an employee object as an argument
 class EmployeeMailer < ApplicationMailer
+  def edit_employee_email(employee)
+    @employee = employee
+    mail(to: @employee.email, subject: 'Profile updated')
+  end
 end
