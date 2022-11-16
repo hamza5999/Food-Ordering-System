1029e383d93c7159fc47b44e7e4eba1ff6fdddb8
hamza5999 <mhamzamahmood5@gmail.com>
2022-11-16 15:27:15 +0500

    Remove MenuItemMenu model

--- app/models/menu_item_menu.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# `MenuItemMenu` is a join table between `Menu` and `MenuItem`
-class MenuItemMenu < ApplicationRecord
-  belongs_to :menu
-  belongs_to :menu_item
-end
