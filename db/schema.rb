a66a92310489f08f3bdf280847fc1d4d3b836b73
hamza5999 <mhamzamahmood5@gmail.com>
2022-11-17 11:16:39 +0500

    Add polymorphic attributes in MenuItems table in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_16_132146) do
+ActiveRecord::Schema.define(version: 2022_11_17_060317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -228,7 +228,10 @@ ActiveRecord::Schema.define(version: 2022_11_16_132146) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "menu_id", null: false
+    t.string "menu_itemable_type", null: false
+    t.bigint "menu_itemable_id", null: false
     t.index ["menu_id"], name: "index_menu_items_on_menu_id"
+    t.index ["menu_itemable_type", "menu_itemable_id"], name: "index_menu_items_on_menu_itemable"
   end
 
   create_table "menu_timings", force: :cascade do |t|
