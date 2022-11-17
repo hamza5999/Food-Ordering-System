2a275b16cb66eff9d32ed4ba929ad4a7ceedebbc
hamza5999 <mhamzamahmood5@gmail.com>
2022-11-17 15:29:31 +0500

    Add index action in MenusController

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
+# The MenusController class inherits from the ApplicationController class, and defines an index action
+# that sets the @menus instance variable to the result of calling Menu.all.
 class MenusController < ApplicationController
+  def index
+    @menus = Menu.all
+  end
 end
