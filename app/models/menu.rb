09110696abc327e6f75f0ec3fae65d60baca76e5
hamza5999 <mhamzamahmood5@gmail.com>
2022-11-17 17:57:06 +0500

    Include discard in Menu model to make it discardable

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,6 +3,8 @@
 # A Menu belongs to a Restaurant, has many MenuTimings, has many MenuItemMenus, and has many MenuItems
 # through MenuItemMenus
 class Menu < ApplicationRecord
+  include Discard::Model
+
   belongs_to :restaurant
   has_many :menu_timings
   has_many :menu_items
