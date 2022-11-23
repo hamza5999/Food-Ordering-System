1ffcb587af4af68067b1e3a3dff5fb2ac634b6b3
hamza5999 <mhamzamahmood5@gmail.com>
2022-11-23 10:57:49 +0500

    Add validation for start_time and end_time presence in MenuTiming model

--- app/models/menu_timing.rb
+++ app/models/menu_timing.rb
@@ -5,4 +5,6 @@ class MenuTiming < ApplicationRecord
   enum days: { monday: 0, tuesday: 1, wednesday: 2, thursday: 3, friday: 4, saturday: 5, sunday: 6 }
 
   belongs_to :menu
+
+  validates :start_time, :end_time, presence: true
 end
