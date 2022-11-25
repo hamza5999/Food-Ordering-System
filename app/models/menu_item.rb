25df3d5ee32b9aa5cb66b61f4ecc0c5b8a881452
hamza5999 <mhamzamahmood5@gmail.com>
2022-11-25 15:06:54 +0500

    Remove belongs_to :restaurant association from MenuItem model

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -3,7 +3,6 @@
 # A MenuItem belongs to a MenuItemable, which is a polymorphic association. It belongs to a Restaurant and Menu
 class MenuItem < ApplicationRecord
   belongs_to :menu_itemable, polymorphic: true
-  belongs_to :restaurant
   belongs_to :menu
 
   has_many :addon_menu_items
